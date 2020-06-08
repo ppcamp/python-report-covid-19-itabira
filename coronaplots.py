52099fe89855cc14b7dbffdfbf00bcc7d88d8c18
ppcamp <p.augustocampos@gmail.com>
2020-06-08 15:14:00 -0300

    Removing outputs

--- coronaplots.py
+++ coronaplots.py
@@ -7,46 +7,6 @@
 # This code focus on readability instead speed. 
 # 
 # > Note, you can remove all filters and others steps by replacing it by a simple loop.
-# %% [markdown]
-# # Downloads
-# This section needs to be executed by system. You can also use a shell script to do this (Just remove '!' and execute).
-# 
-# ```bash
-# # Download tool to generate pdf Report
-# pip3 -q install reportlab > /dev/null
-# 
-# # Download tools to login and get spreadsheet
-# pip3 -q install oauth2client > /dev/null
-# pip3 -q install gspread > /dev/null
-# 
-# # Download tools to threat data
-# pip3 -q install pandas > /dev/null
-# pip3 -q install numpy > /dev/null
-# 
-# # Download tools to plot
-# pip3 -q install matplotlib > /dev/null
-# pip3 -q install seaborn > /dev/null
-# 
-# 
-# # Download font that will be used
-# mkdir fonts
-# curl https://fonts.google.com/download?family=Montserrat -so font.zip
-# unzip -qq font.zip -d fonts && rm font.zip
-# 
-# # Download Images
-# #     IMG (Imgur keeps images if 1 view per six months)
-# #     https://i.imgur.com/vKgu7bE.png --> WomanBlue
-# #     https://i.imgur.com/GcsTCOp.png --> Default Itabira's logo
-# wget http://www.clker.com/cliparts/2/a/Q/D/P/J/woman-orange-hi.png -qO girl.png
-# wget https://i.imgur.com/OnzjeAY.png -qO boy.png
-# mkdir img && mv boy.png img/boy.png && mv girl.png img/girl.png
-# wget https://i.imgur.com/ef1IAWY.jpg -qO img/logo.png
-# 
-# # Create folder to store pdf
-# mkdir pdfs/
-# mkdir logs/
-# ```
-
 # %%
 # -*- coding: utf-8 -*-
 # get_ipython().run_line_magic('config', 'IPCompleter.greedy=True')
